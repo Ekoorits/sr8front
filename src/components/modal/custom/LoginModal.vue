6c1e429203955e7aead24b6916145669a6194781
Ekoorits <enrikoorits@gmail.com>
2025-12-04 13:22:47 +0200

    login impl WIP

--- src/components/modal/custom/LoginModal.vue
+++ src/components/modal/custom/LoginModal.vue
@@ -1,7 +1,17 @@
 <template>
   <Modal :modal-is-open="loginModalIsOpen">
     <template #title>
-      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      Sisselogimine
+    </template>
+    <template #body>
+      <div class="form-floating mb-3">
+        <input v-model="username" type="text" class="form-control" placeholder="Kasutajanimi">
+        <label>Kasutajanimi</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input v-model="password" type="password" class="form-control" placeholder="Parool">
+        <label>Parool</label>
+      </div>
     </template>
   </Modal>
 </template>
